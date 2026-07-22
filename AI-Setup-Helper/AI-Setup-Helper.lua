f2cea1c0f7f181e9f8eef63640b601729e6d7b70
jcsilva7 <josemiguelcsilva@gmail.com>
2026-07-22 23:13:09 +0100

    Initial commit

---
+++ AI-Setup-Helper/AI-Setup-Helper.lua
@@ -0,0 +1,22 @@
+local car
+
+-- UI functions
+local UI = require("ui")
+
+function script.onSessionStart(sessionIndex)
+
+end
+
+function script.update(dt)
+    
+end
+
+-- Main part
+function script.windowMain(dt)
+    
+end
+
+-- Settings UI
+function script.windowSettings()
+    -- TODO: change thresholds
+end
