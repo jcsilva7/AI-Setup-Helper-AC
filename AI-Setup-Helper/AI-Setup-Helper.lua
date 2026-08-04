564d1de68bb44b2b47c559463e78ddd29ec7bc78
jcsilva7 <josemiguelcsilva@gmail.com>
2026-08-04 12:02:09 +0100

    Make UI

--- AI-Setup-Helper/AI-Setup-Helper.lua
+++ AI-Setup-Helper/AI-Setup-Helper.lua
@@ -11,25 +11,17 @@ local c_json = require("custom_json")
 -- dotenv.load()
 
 --- Get all the sim data required to make the setup
----@param include_temps boolean      If the track and air temp should be considered
----@param include_weather boolean    If the weather should be considered
 ---@return _ string                  The json with all the data
-local function get_sim_data(include_temps, include_weather)
+local function get_sim_data()
     if !ac.isSetupAvailableToEdit() then
         return json.encode({})
     end
 
-    local track_temp = nil
-    local air_temp = nil
-    if include_temps then
-        track_temp = sim.roadTemperature
-        air_temp = sim.ambientTemperature
-    end
+    -- Temps and weather
+    local track_temp = sim.roadTemperature
+    local air_temp = sim.ambientTemperature
 
-    local weather = nil
-    if include_weather then
-        weather = sim.weatherConditions
-    end
+    local weather = sim.weatherConditions
     
     -- Get all available setup data and turn it into a json
     local setup_data = ac.getSetupSpinners()
@@ -69,6 +61,64 @@ end
 
 -- Main part
 function script.windowMain(dt)
-    -- TODO: Fuel
-    -- TODO: UI
+    local setup_data = get_sim_data()
+    local request_status = nil
+
+    local oversteer = false
+    local understeer = false
+
+    local includeTrackTemp = false
+    local includeAirTemp = false
+    local includeWeather = false
+    local includeFuel = false
+
+    -- CONDITIONS --------------------------------------------------
+    ui.text('Conditions')
+    ui.separator()
+    
+    if ui.checkbox('Track temp', includeTrackTemp) then includeTrackTemp = not includeTrackTemp end
+    if includeTrackTemp then
+        setup_data.track_temp = ui.slider('##trackTemp', setup_data.track_temp, 0, 80, 'Track: %.0f°C')
+    else
+        setup_data.track_temp = nil
+    end
+    
+    if ui.checkbox('Air temp', includeAirTemp) then includeAirTemp = not includeAirTemp end
+    if includeAirTemp then
+        setup_data.air_temp = ui.slider('##airTemp', setup_data.air_temp, 0, 60, 'Air: %.0f°C')
+    else
+        setup_data.air_temp = nil
+    end
+    
+    if ui.checkbox('Weather', includeWeather) then includeWeather = not includeWeather end
+    if ~includeWeather then
+        setup_data.weather = nil
+    end
+    
+    if ui.checkbox('Fuel', includeFuel) then includeFuel = not includeFuel end
+    if includeFuel then
+        setup_data["laps_fuel"] = ui.slider('##fuel', 20, 1, 99, 'Fuel: %.0f laps')        
+    end
+    
+    -- FIX -----------------------------------------------------------
+    ui.newLine()
+    ui.text('Fix')
+    ui.separator()
+    
+    if ui.checkbox('Oversteer', oversteer) then oversteer = not oversteer end
+    if ui.checkbox('Understeer', understeer) then understeer = not understeer end
+    
+    -- REQUEST ---------------------------------------------------------
+    ui.newLine()
+    if ui.button('Request', vec2(-1, 0)) then
+        -- TODO: make request
+    end
+    
+    if request_status == nil then
+        ui.text('Awaiting request')
+    elseif request_status == 'success' then
+        ui.textColored('Request acknowledged', rgbm.colors.green)
+    else
+        ui.textColored('No response — retry', rgbm.colors.red)
+    end
 end
