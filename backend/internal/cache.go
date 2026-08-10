a235093ead992c08e3b513a5433c3e620d0aea92
jcsilva7 <josemiguelcsilva@gmail.com>
2026-08-10 16:37:36 +0100

    shady cache functionality

--- backend/internal/cache.go
+++ backend/internal/cache.go
@@ -1,29 +1,51 @@
 package internal
 
 import (
+	"math"
 	"sync"
 	"time"
 )
 
+// TempsThreshold
+// The threshold to give cached setups within this range (in ºC)
+const TempsThreshold = 3
+
+// Entry
+// Cache entry, where the setup data is stored
 type Entry struct {
-	createdAt time.Time
-	data      []byte
+	createdAt  time.Time
+	data       []byte
+	comparison ShadyComparison
 }
 
+// Key
+// Cache key, to identify the setups based on the car and track configurations
 type Key struct {
 	Car         string `json:"car_name"`
 	Track       string `json:"track_name"`
 	TrackLayout string `json:"layout_name"`
-	AirTemp     string `json:"air_temp,omitempty"`
-	TrackTemp   string `json:"track_temp,omitempty"`
-	Weather     string `json:"weather,omitempty"`
 }
 
+// Cache
+// Full cache structure for the app
 type Cache struct {
 	entries map[Key]Entry
 	mut     sync.RWMutex
 }
 
+// ShadyComparison
+// If the cached setup has the same weather and close temps,
+// It is not like the LLM has got it right for the original,
+// so it is better to just give the cached for similar temps
+type ShadyComparison struct {
+	AirTemp   int    `json:"air_temp"`
+	TrackTemp int    `json:"track_temp"`
+	Weather   string `json:"weather"`
+}
+
+// NewCache
+// Creates and returns a new empty cache
+// Takes the time to live (ttl) of the entries, and the interval between cache reaps (clean old entries)
 func NewCache(ttl, reapInterval time.Duration) *Cache {
 	c := &Cache{entries: make(map[Key]Entry)}
 
@@ -32,27 +54,20 @@ func NewCache(ttl, reapInterval time.Duration) *Cache {
 	return c
 }
 
-func (c *Cache) Get(key Key) ([]byte, bool) {
-	c.mut.RLock()
-	defer c.mut.RUnlock()
-
-	if entry, ok := c.entries[key]; ok {
-		return entry.data, true
-	}
-
-	return []byte{}, false
-}
-
-func (c *Cache) Put(key Key, data []byte) {
+// Put
+// Add a new entry to the cache
+func (c *Cache) Put(key Key, comparison ShadyComparison, data []byte) {
 	c.mut.Lock()
 	defer c.mut.Unlock()
 
 	c.entries[key] = Entry{
-		createdAt: time.Now(),
-		data:      data,
+		createdAt:  time.Now(),
+		data:       data,
+		comparison: comparison,
 	}
 }
 
+// Clean old entries based on the ttl
 func (c *Cache) reap(ttl, reapInterval time.Duration) {
 	ticker := time.NewTicker(reapInterval)
 	defer ticker.Stop()
@@ -68,4 +83,29 @@ func (c *Cache) reap(ttl, reapInterval time.Duration) {
 
 		c.mut.Unlock()
 	}
+}
+
+// GetCacheSetup
+// Exposed function (the one to be used)
+// Returns a cached setup, if the key exists and the temps are within the range (or are nil)
+func (c *Cache) GetCacheSetup(key Key, comparison ShadyComparison) []byte {
+	c.mut.RLock()
+	cachedSetup, hit := c.entries[key]
+	c.mut.RUnlock()
+	if !hit {
+		return nil
+	}
+
+	// Check the weather first, if one is for dry and another for wet, do not bother
+	if comparison.Weather != "" && comparison.Weather != cachedSetup.comparison.Weather {
+		return nil
+	}
+
+	if comparison.TrackTemp == -1 || math.Abs(float64(comparison.TrackTemp-cachedSetup.comparison.TrackTemp)) <= TempsThreshold {
+		if comparison.AirTemp == -1 || math.Abs(float64(comparison.AirTemp-cachedSetup.comparison.AirTemp)) <= TempsThreshold {
+			return cachedSetup.data
+		}
+	}
+
+	return nil
 }
