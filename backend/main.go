c9ac269af31c96e7b79bad173cdd5cd125caccda
jcsilva7 <josemiguelcsilva@gmail.com>
2026-08-21 21:23:25 +0100

    fix log after serving request

--- backend/main.go
+++ backend/main.go
@@ -284,11 +284,12 @@ func middleware(next http.Handler) http.Handler {
 			return
 		}
 
+		log.Println("Request Received...")
+
 		// Limit body size read
 		r.Body = http.MaxBytesReader(w, r.Body, maxBodySize*1024)
 
 		next.ServeHTTP(w, r)
-		log.Println("Request Received...")
 	})
 }
 
